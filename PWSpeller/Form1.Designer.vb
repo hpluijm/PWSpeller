ba7407361374d285a9a22cef378fe5f1b58a1124
hpluijm <hans@pluijm.eu>
2017-01-03 23:58:02 +0100

    Volledig werkende versie 1.0

--- PWSpeller/Form1.Designer.vb
+++ PWSpeller/Form1.Designer.vb
@@ -22,17 +22,183 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.numPasswdSize = New System.Windows.Forms.NumericUpDown()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.chkAZCap = New System.Windows.Forms.CheckBox()
+        Me.chkAZlow = New System.Windows.Forms.CheckBox()
+        Me.chk09 = New System.Windows.Forms.CheckBox()
+        Me.chkSPC = New System.Windows.Forms.CheckBox()
+        Me.txtSpelled = New System.Windows.Forms.TextBox()
+        Me.btnSpell = New System.Windows.Forms.Button()
+        Me.grdTeksten = New System.Windows.Forms.DataGridView()
+        Me.cmbTaal = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.numPasswdSize, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.grdTeksten, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Location = New System.Drawing.Point(429, 72)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(75, 23)
+        Me.btnGenerate.TabIndex = 0
+        Me.btnGenerate.Text = "Generate"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'numPasswdSize
+        '
+        Me.numPasswdSize.Location = New System.Drawing.Point(364, 74)
+        Me.numPasswdSize.Maximum = New Decimal(New Integer() {20, 0, 0, 0})
+        Me.numPasswdSize.Name = "numPasswdSize"
+        Me.numPasswdSize.Size = New System.Drawing.Size(59, 20)
+        Me.numPasswdSize.TabIndex = 2
+        Me.numPasswdSize.Value = New Decimal(New Integer() {8, 0, 0, 0})
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(282, 77)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(76, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Password Size"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(12, 74)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(247, 20)
+        Me.txtPassword.TabIndex = 4
+        '
+        'chkAZCap
+        '
+        Me.chkAZCap.AutoSize = True
+        Me.chkAZCap.Checked = True
+        Me.chkAZCap.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.chkAZCap.Location = New System.Drawing.Point(12, 33)
+        Me.chkAZCap.Name = "chkAZCap"
+        Me.chkAZCap.Size = New System.Drawing.Size(43, 17)
+        Me.chkAZCap.TabIndex = 5
+        Me.chkAZCap.Text = "A-Z"
+        Me.chkAZCap.UseVisualStyleBackColor = True
+        '
+        'chkAZlow
+        '
+        Me.chkAZlow.AutoSize = True
+        Me.chkAZlow.Checked = True
+        Me.chkAZlow.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.chkAZlow.Location = New System.Drawing.Point(61, 33)
+        Me.chkAZlow.Name = "chkAZlow"
+        Me.chkAZlow.Size = New System.Drawing.Size(40, 17)
+        Me.chkAZlow.TabIndex = 6
+        Me.chkAZlow.Text = "a-z"
+        Me.chkAZlow.UseVisualStyleBackColor = True
+        '
+        'chk09
+        '
+        Me.chk09.AutoSize = True
+        Me.chk09.Checked = True
+        Me.chk09.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.chk09.Location = New System.Drawing.Point(107, 33)
+        Me.chk09.Name = "chk09"
+        Me.chk09.Size = New System.Drawing.Size(41, 17)
+        Me.chk09.TabIndex = 7
+        Me.chk09.Text = "0-9"
+        Me.chk09.UseVisualStyleBackColor = True
+        '
+        'chkSPC
+        '
+        Me.chkSPC.AutoSize = True
+        Me.chkSPC.Location = New System.Drawing.Point(154, 33)
+        Me.chkSPC.Name = "chkSPC"
+        Me.chkSPC.Size = New System.Drawing.Size(77, 17)
+        Me.chkSPC.TabIndex = 8
+        Me.chkSPC.Text = "!@#$%^&*()"
+        Me.chkSPC.UseVisualStyleBackColor = True
+        '
+        'txtSpelled
+        '
+        Me.txtSpelled.Location = New System.Drawing.Point(26, 141)
+        Me.txtSpelled.Multiline = True
+        Me.txtSpelled.Name = "txtSpelled"
+        Me.txtSpelled.Size = New System.Drawing.Size(488, 257)
+        Me.txtSpelled.TabIndex = 9
+        '
+        'btnSpell
+        '
+        Me.btnSpell.Location = New System.Drawing.Point(429, 101)
+        Me.btnSpell.Name = "btnSpell"
+        Me.btnSpell.Size = New System.Drawing.Size(75, 23)
+        Me.btnSpell.TabIndex = 10
+        Me.btnSpell.Text = "Spell"
+        Me.btnSpell.UseVisualStyleBackColor = True
+        '
+        'grdTeksten
+        '
+        Me.grdTeksten.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grdTeksten.Location = New System.Drawing.Point(138, 177)
+        Me.grdTeksten.Name = "grdTeksten"
+        Me.grdTeksten.Size = New System.Drawing.Size(240, 150)
+        Me.grdTeksten.TabIndex = 11
+        '
+        'cmbTaal
+        '
+        Me.cmbTaal.FormattingEnabled = True
+        Me.cmbTaal.Location = New System.Drawing.Point(364, 33)
+        Me.cmbTaal.Name = "cmbTaal"
+        Me.cmbTaal.Size = New System.Drawing.Size(140, 21)
+        Me.cmbTaal.TabIndex = 12
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(267, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(91, 13)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "Spelling language"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(545, 410)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmbTaal)
+        Me.Controls.Add(Me.btnSpell)
+        Me.Controls.Add(Me.txtSpelled)
+        Me.Controls.Add(Me.chkSPC)
+        Me.Controls.Add(Me.chk09)
+        Me.Controls.Add(Me.chkAZlow)
+        Me.Controls.Add(Me.chkAZCap)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.numPasswdSize)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.Controls.Add(Me.grdTeksten)
         Me.Name = "Form1"
         Me.Text = "PWSpeller"
+        CType(Me.numPasswdSize, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.grdTeksten, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents btnGenerate As Button
+    Friend WithEvents numPasswdSize As NumericUpDown
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents chkAZCap As CheckBox
+    Friend WithEvents chkAZlow As CheckBox
+    Friend WithEvents chk09 As CheckBox
+    Friend WithEvents chkSPC As CheckBox
+    Friend WithEvents txtSpelled As TextBox
+    Friend WithEvents btnSpell As Button
+    Friend WithEvents grdTeksten As DataGridView
+    Friend WithEvents cmbTaal As ComboBox
+    Friend WithEvents Label1 As Label
 End Class
--- PWSpeller/Form1.vb
+++ PWSpeller/Form1.vb
@@ -1,3 +1,172 @@
-﻿Public Class Form1
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Text
+Imports System.Security.Cryptography
+Imports System.Windows.Forms
 
+Public Class Form1
+    Private randomBytes() As Byte
+    Private randomInt32Value As Integer
+    Private possibleChars As String
+    Private possibleChars1 As String
+    Private possibleChars2 As String
+    Private possibleChars3 As String
+    Private possibleChars4 As String
+    Private len As Int32
+    Private GetRandomInt32Value As New RandomInt32Value
+    Private GetPasswordGenProfiler As New PasswordGenProfiler
+    Private sData() As String
+    Private rowTel, colTel As Long
+    
+    Public Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        possibleChars1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        possibleChars2 = "abcdefghijklmnopqrstuvwxyz"
+        possibleChars3 = "0123456789"
+        possibleChars4 = "!@#$%^&*()_+"
+        LeesCSV()
+    End Sub
+
+    Private Sub btnGenerate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGenerate.Click
+        len = numPasswdSize.Value
+        possibleChars = ""
+        If chkAZCap.Checked Then possibleChars = possibleChars & possibleChars1
+        If chkAZlow.Checked Then possibleChars = possibleChars & possibleChars2
+        If chk09.Checked Then possibleChars = possibleChars & possibleChars3
+        If chkSPC.Checked Then possibleChars = possibleChars & possibleChars4
+        Try
+            Dim cpossibleChars() As Char
+            cpossibleChars = possibleChars.ToCharArray()
+            If cpossibleChars.Length < 1 Then
+                MessageBox.Show("You must enter one or more possible characters.")
+                Return
+            End If
+            If len < 4 Then
+                MessageBox.Show(String.Format("Please choose a password length. That length must be a value between {0} and {1}. Note: values above 1,000 might take a LONG TIME to process on some computers.", 4, Int32.MaxValue))
+                Return
+            End If
+
+            Dim builder As New StringBuilder()
+
+            For i As Integer = 0 To len - 1
+                Dim randInt32 As Integer = GetRandomInt32Value.GetRandomInt()
+                Dim r As New Random(randInt32)
+
+                Dim nextInt As Integer = r.[Next](cpossibleChars.Length)
+                Dim c As Char = cpossibleChars(nextInt)
+                builder.Append(c)
+            Next
+            Me.txtPassword.Text = builder.ToString()
+            fill_txtSpelled(Me.txtPassword.Text)
+        Catch ex As Exception
+            MessageBox.Show(String.Format("An error has occurred while trying to generate random password! Technical description: {0}", ex.Message.ToString()))
+        End Try
+
+    End Sub
+    Private Sub fill_txtSpelled(pswd As String)
+        Dim lenTekst As Integer
+        Dim chrTeller As Integer
+        Dim indx As Long
+        Dim pswSpelled As String
+        lenTekst = pswd.Length
+        pswSpelled = ""
+        For chrTeller = 1 To lenTekst
+            indx = Asc(Mid(pswd, chrTeller, 1)) - 1
+            If chrTeller > 1 Then pswSpelled = pswSpelled & vbCrLf
+            pswSpelled = pswSpelled & grdTeksten.Rows(indx).Cells(cmbTaal.SelectedIndex).Value
+        Next
+        Me.txtSpelled.Text = pswSpelled
+    End Sub
+
+    Private Sub btnSpell_Click(sender As Object, e As EventArgs) Handles btnSpell.Click
+        fill_txtSpelled(Me.txtPassword.Text)
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub LeesCSV()
+        Dim path As String
+        path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase).Replace("\bin\Debug", "")
+        path = path.Replace("file:\", "")
+        ' Using ioReader As New Microsoft.VisualBasic.FileIO.TextFieldParser(path & "D:\Files\Sources\Repos\PWSpeller\PWSpeller\talen.csv")
+        Using ioReader As New Microsoft.VisualBasic.FileIO.TextFieldParser(path & "\talen.csv")
+            ioReader.TextFieldType = FileIO.FieldType.Delimited
+            ioReader.SetDelimiters(",")
+
+            Dim TextFileTable As DataTable = Nothing
+            Dim Column As DataColumn
+            Dim Row As DataRow
+            Dim UpperBound As Int32
+            Dim ColumnCount As Int32
+            Dim CurrentRow As String()
+            While Not ioReader.EndOfData
+                Try
+                    CurrentRow = ioReader.ReadFields()
+                    If Not CurrentRow Is Nothing Then
+                        If TextFileTable Is Nothing Then
+                            cmbTaal.Items.Clear()
+                            TextFileTable = New DataTable("TextFileTable")
+                            UpperBound = CurrentRow.GetUpperBound(0)
+                            For ColumnCount = 0 To UpperBound
+                                Column = New DataColumn()
+                                Column.DataType = System.Type.GetType("System.String")
+                                Column.ColumnName = "Column" & ColumnCount
+                                Column.Caption = "Column" & ColumnCount
+                                Column.ReadOnly = True
+                                Column.Unique = False
+                                cmbTaal.Items.Add(CurrentRow(ColumnCount).ToString)
+                                TextFileTable.Columns.Add(Column)
+                            Next
+                        End If
+                        Row = TextFileTable.NewRow
+                        For ColumnCount = 0 To UpperBound
+                            Row("Column" & ColumnCount) = CurrentRow(ColumnCount).ToString
+                        Next
+                        TextFileTable.Rows.Add(Row)
+                    End If
+                Catch ex As Microsoft.VisualBasic.FileIO.MalformedLineException
+                    MsgBox("Line " & ex.Message & "is not valid and will be skipped.")
+                End Try
+            End While
+            ioReader.Dispose()
+            grdTeksten.DataSource = TextFileTable
+            cmbTaal.SelectedIndex = 0
+        End Using
+    End Sub
+End Class
+
+
+Public Class PasswordGenProfiler
+    Public Shared Function GetFrequencyDistributionOfChars(allowableChars As String, generatedPass As String) As Dictionary(Of Char, Integer)
+        Dim distrib As New Dictionary(Of Char, Integer)()
+        ' initialize all values to 0
+        For Each c As Char In allowableChars
+            ' If character is listed more than once, don't re-add it to our list.
+            If Not distrib.ContainsKey(c) Then
+                distrib.Add(c, 0)
+            End If
+        Next
+        Dim val As Integer = 0
+        For Each passChar As Char In generatedPass
+            If distrib.TryGetValue(passChar, val) Then
+                distrib(passChar) = System.Threading.Interlocked.Increment(val)
+            End If
+        Next
+
+        Return distrib
+    End Function
+End Class
+Public Class RandomInt32Value
+    Public Function GetRandomInt() As Integer
+        Dim randomBytes As Byte() = New Byte(3) {}
+        Dim rng As New RNGCryptoServiceProvider()
+        rng.GetBytes(randomBytes)
+        Dim randomInt As Integer = BitConverter.ToInt32(randomBytes, 0)
+        Return randomInt
+    End Function
 End Class
